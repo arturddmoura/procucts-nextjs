77320e07f5305b203e3059dab157d1f41fda039a
Artur Moura <artur.dornelles@gmail.com>
2023-01-18 19:04:32 -0300

    pag inicial com get

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useState } from 'react';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    const [queryClient] = useState(() => new QueryClient());
+    return (
+        // Provide the client to your App
+        <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+        </QueryClientProvider>
+    );
 }
